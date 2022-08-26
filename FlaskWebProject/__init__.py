8fcf821e87f04ee22b624d8cf28279e1a6211813
Shubham Choudhary <jchoudhary1994@gmail.com>
2022-08-26 22:06:19 +0530

    final changes

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -1,6 +1,7 @@
 """
 The flask application package.
 """
+
 import logging
 from flask import Flask
 from config import Config
@@ -10,10 +11,15 @@ from flask_session import Session
 
 app = Flask(__name__)
 app.config.from_object(Config)
-# TODO: Add any logging levels and handlers with app.logger
+#  Add any logging levels and handlers with app.logger
+app.logger.setLevel(logging.INFO)
+streamHandler = logging.StreamHandler()
+streamHandler.setLevel(logging.INFO)
+app.logger.addHandler(streamHandler)
 Session(app)
 db = SQLAlchemy(app)
 login = LoginManager(app)
 login.login_view = 'login'
+
 
 import FlaskWebProject.views
