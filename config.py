78ac42d503b59356292fc84eab5d825b0636cb87
Shubham Choudhary <jchoudhary1994@gmail.com>
2022-08-26 16:58:45 +0530

    modified config

--- config.py
+++ config.py
@@ -5,21 +5,26 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret-key'
 
-    BLOB_ACCOUNT = os.environ.get('BLOB_ACCOUNT') or 'ENTER_STORAGE_ACCOUNT_NAME'
-    BLOB_STORAGE_KEY = os.environ.get('BLOB_STORAGE_KEY') or 'ENTER_BLOB_STORAGE_KEY'
-    BLOB_CONTAINER = os.environ.get('BLOB_CONTAINER') or 'ENTER_IMAGES_CONTAINER_NAME'
-
-    SQL_SERVER = os.environ.get('SQL_SERVER') or 'ENTER_SQL_SERVER_NAME.database.windows.net'
-    SQL_DATABASE = os.environ.get('SQL_DATABASE') or 'ENTER_SQL_DB_NAME'
-    SQL_USER_NAME = os.environ.get('SQL_USER_NAME') or 'ENTER_SQL_SERVER_USERNAME'
-    SQL_PASSWORD = os.environ.get('SQL_PASSWORD') or 'ENTER_SQL_SERVER_PASSWORD'
+    BLOB_ACCOUNT = os.environ.get(
+        'BLOB_ACCOUNT') or 'udacitynanostorage'
+    BLOB_STORAGE_KEY = os.environ.get(
+        'BLOB_STORAGE_KEY') or 'e5NTwY2KdDANfinS0bHfZjw+A0IqPvqTj5ftuEMiyoBkV5kgveJyQ/i9YBfEUJfeXYzOh2dM2rBX+AStsb/NYw=='
+    BLOB_CONTAINER = os.environ.get(
+        'BLOB_CONTAINER') or 'images'
+    SQL_SERVER = os.environ.get(
+        'SQL_SER') or 'udacity-nano-degree-server.database.windows.net'
+    SQL_DATABASE = os.environ.get('SQL_DATABASE') or 'udacitynanodegree-db'
+    SQL_USER_NAME = os.environ.get(
+        'SQL_USER_NAME') or 'adminudacity'
+    SQL_PASSWORD = os.environ.get(
+        'SQL_PASSWORD') or 'P@ssword1'
     # Below URI may need some adjustments for driver version, based on your OS, if running locally
     SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://' + SQL_USER_NAME + '@' + SQL_SERVER + ':' + SQL_PASSWORD + '@' + SQL_SERVER + ':1433/' + SQL_DATABASE  + '?driver=ODBC+Driver+17+for+SQL+Server'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    ### Info for MS Authentication ###
+        ### Info for MS Authentication ###
     ### As adapted from: https://github.com/Azure-Samples/ms-identity-python-webapp ###
-    CLIENT_SECRET = "ENTER_CLIENT_SECRET_HERE"
+    CLIENT_SECRET = "u9m8Q~PbUvpv3LMPBS8M04.marxrURCnHRa_KaHh"
     # In your production app, Microsoft recommends you to use other ways to store your secret,
     # such as KeyVault, or environment variable as described in Flask's documentation here:
     # https://flask.palletsprojects.com/en/1.1.x/config/#configuring-from-environment-variables
@@ -27,15 +32,13 @@ class Config(object):
     # if not CLIENT_SECRET:
     #     raise ValueError("Need to define CLIENT_SECRET environment variable")
 
-    AUTHORITY = "https://login.microsoftonline.com/common"  # For multi-tenant app, else put tenant name
+    # For multi-tenant app, else put tenant name
+    AUTHORITY = "https://login.microsoftonline.com/common"
     # AUTHORITY = "https://login.microsoftonline.com/Enter_the_Tenant_Name_Here"
 
-    CLIENT_ID = "ENTER_CLIENT_ID_HERE"
+    CLIENT_ID = "dad03134-e222-4cfc-b1ce-4baf3b83e556"
 
-    REDIRECT_PATH = "/getAToken"  # Used to form an absolute URL; must match to app's redirect_uri set in AAD
-
-    # You can find the proper permission names from this document
-    # https://docs.microsoft.com/en-us/graph/permissions-reference
-    SCOPE = ["User.Read"] # Only need to read user profile for this app
+    # Used to form an absolute URL; must match to app's redirect_uri set in AAD
+    REDIRECT_PATH = "/auth-token"
 
     SESSION_TYPE = "filesystem"  # Token cache will be stored in server-side session
